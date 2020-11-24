1125cbb8b1f457f5e9f0711d41d1ff6e0eb5c09a
Gefgs <62507867+Gefgs@users.noreply.github.com>
2020-11-24 10:08:43 +0300

    fix: common update

--- src/CalendarApiApplicationCore/Constans/Common.cs
+++ src/CalendarApiApplicationCore/Constans/Common.cs
@@ -7,5 +7,6 @@ namespace CalendarApiApplicationCore.Constans
     internal class Common
     {
         public const string Url = "https://api.abalin.net/getdate?name=John&country=us";
+        //
     }
 }
